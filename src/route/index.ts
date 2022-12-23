9e6a594ad6c8565ee028ac20081531de39d2a935
devanandgiri-respo <devanand.giri@respo.co.in>
2022-12-23 11:17:24 +0530

    second commit

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,9 +1,24 @@
 import express, {Request, Response, } from "express"
+import app from "../app";
 
 const router = express.Router()
 
 router.get("/", (req: Request, res: Response, next: Function): void =>{
     res.status(200).json({message: "User is Created"})
 });
+
+router.get("/user/:id", (req: Request, res: Response, next: Function): void =>{
+    let id = req.params.id
+    let a = async ()=>{
+        let result 
+        if(id=='all')
+        result = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
+        else
+        result = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+        let rrr = await result.json();
+        res.send(rrr)
+    }
+    a()
+})
 
 export default router;
